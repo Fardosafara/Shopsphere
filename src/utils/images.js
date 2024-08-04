f4044b1e9a3167ef26d476ce963510763d698ebc
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-08-04 20:50:22 +0300

    first commit

---
+++ src/utils/images.js
@@ -0,0 +1,8 @@
+import slider_img_1 from "../assets/images/Red Paper New Arrival Flyer.png";
+//import slider_img_2 from "../assets/images/slider_img_2.jpg";
+import shopping_cart from "../assets/images/shopping_cart.png";
+import correct from "../assets/images/correct.png";
+import loader from "../assets/images/loader.svg";
+ 
+const sliderImgs = [slider_img_1];
+export {sliderImgs, shopping_cart, correct, loader};
